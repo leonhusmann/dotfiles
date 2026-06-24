0f9dcc562d1dd6b2cae8ad3f20e3fb2e1b9ca2d6
Leon Husmann <leon.husmann@check24.de>
2026-06-24 10:25:34 +0200

    fix bitbucket mcp

--- modules/mcp/bitbucket.nix
+++ modules/mcp/bitbucket.nix
@@ -10,9 +10,10 @@
       BITBUCKET_URL = "https://api.bitbucket.org/2.0";
       BITBUCKET_WORKSPACE = "check24";
       BITBUCKET_ENABLE_DANGEROUS = "false";
+      BITBUCKET_USERNAME = "leon.husmann@check24.de";
     };
     secretEnvironment = {
-      BITBUCKET_TOKEN = "op://Employee/c4hnroxib6gen6uzu35u7hm3g4/password";
+      BITBUCKET_PASSWORD = "op://Employee/yqb5lp4bof5c2lg7o6v4pn2due/password";
     };
   };
 }
